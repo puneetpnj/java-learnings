1a7fa31551f7c4e81bf45227736e7fd796e7c076
puneetpnj <nandanjha.puneet@gmail.com>
2021-10-13 00:49:36 +0530

    added .gitignore ffile to ignore .class files

--- core-java/cars.java
+++ core-java/cars.java
@@ -4,20 +4,20 @@ class chevrolet {
         System.out.println("Chevrolet wheels has michelin tyres");
     }
 }
-    class corvette extends chevrolet {
-        void engine ()
-        {
+class corvette extends chevrolet {
+    void engine ()
+    {
             System.out.println("Corvette cars have v8 engines");
-        }
     }
-        class singleinheritance {
-            public static void main (String[]args){
-                corvette c= new corvette();
-                c.wheels ();
-                c.engine ();
+}
+class singleinheritance {
+    public static void main (String[]args){
+        corvette c= new corvette();
+        c.wheels ();
+        c.engine ();
 
-            }
         }
+}
 
 
 
